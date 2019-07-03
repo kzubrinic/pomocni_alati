fdad217fe3c9f812cdcde8c614435717e35be008
Krunoslav Zubrinic <kruno.zubrinic@gmail.com>
2019-07-03 17:19:14 +0200

    Dorada skripte compresspdf.sh da automatski stvara mapu rez
    	modified:   compresspdf.sh

--- compresspdf.sh
+++ compresspdf.sh
@@ -1,3 +1,11 @@
+# Kompresira sve PDF datoteke iz mape pomoću ps2pdf skripte ghostscripta.
+# Kompresirane datoteke smješta u mapu rez, a izvorne briše.
+#
+# Primjena: ./compresspdf.sh 
+#
+if [ ! -d rez ]; then
+    mkdir rez
+fi
 for FILE in *.pdf
 do
    if test -f "$FILE"; then
